809f43599d80a016fc0f3c23305bc9c7b75bd09d
Ishmeet Kaur <ishmeetkaur364@gmail.com>
2024-08-31 15:17:19 +0000

    tasks

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("connection established");
+// var state = {
+//     taskList: [
+//          {
+//             imageUrl : "",
+//             taskTitle : "",
+//             taskType : "",
+//             taskDesciption : "",
+//          },
+//          {
+//             imageUrl : "",
+//             taskTitle : "",
+//             taskType : "",
+//             taskDesciption : "",
+//          },
+//          {
+//             imageUrl : "",
+//             taskTitle : "",
+//             taskType : "",
+//             taskDesciption : "",
+//          },
+//          {
+//             imageUrl : "",
+//             taskTitle : "",
+//             taskType : "",
+//             taskDesciption : "",
+//          },
+//          {
+//             imageUrl : "",
+//             taskTitle : "",
+//             taskType : "",
+//             taskDesciption : "",
+//          },
+//     ]
+// };
+
+const state = {
+    taskList: [],
+};
+
+// DOM Operations
+const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task__modal__body");
+
+console.log(taskContents);
+console.log(taskModal);
